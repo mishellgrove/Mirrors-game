37546d5c406b964212d914b5cbca6573cef51241
mishellgrove <70043762+mishellgrove@users.noreply.github.com>
2020-11-04 10:23:07 -0500

    creation of the matrix, rows and columns

--- src/model/MatrixGame.java
+++ src/model/MatrixGame.java
@@ -13,7 +13,6 @@ public class MatrixGame {
 	private Cell first;
 	private Player player;
 
-	
 	public MatrixGame(String name, int rows, int cols, int mirror) {
 		this.rows = rows;
 		this.cols = cols;
@@ -24,8 +23,12 @@ public class MatrixGame {
 		this.mirror = mirror;
 		player = new Player(name, 0);
 
+
+		createMatrix();
+		initializeMirror(mirror, rows, cols);
+
 	}
-	
+
 	public int getRows() {
 		return rows;
 	}
@@ -49,7 +52,7 @@ public class MatrixGame {
 	public void setFirst(Cell first) {
 		this.first = first;
 	}
-	
+
 	public int getContador() {
 		return contador;
 	}
@@ -57,7 +60,7 @@ public class MatrixGame {
 	public void setContador(int contador) {
 		this.contador = contador;
 	}
-	
+
 	public int getContadora() {
 		return contadora;
 	}
@@ -65,7 +68,7 @@ public class MatrixGame {
 	public void setContadora(int contadora) {
 		this.contadora = contadora;
 	}
-	
+
 	public int getMirrorCon() {
 		return mirrorCon;
 	}
@@ -81,7 +84,7 @@ public class MatrixGame {
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
-	
+
 	public int getMirror() {
 		return mirror;
 	}
@@ -97,7 +100,68 @@ public class MatrixGame {
 	public void setDisparos(int disparos) {
 		this.disparos = disparos;
 	}
-
 	
+	public void createMatrix() {
+		first = new Cell(0,'A');
+		createRow(0, 0, first);
+	}
 
-}
+	public void createCol(int r, int c, Cell prev) {
+		if(c < cols) {
+			Cell current = new Cell(r, (char)('A'+c));
+			prev.setNext(current);
+			current.setPrev(prev);
+			createCol(r,c+1,current);
+		}
+	}
+
+	public void createRow(int r, int c, Cell firstNode) {
+		createCol(r,c+1,firstNode);
+
+		if(r+1 < rows) {
+			Cell currentDown = new Cell(r+1,'A');
+			currentDown.setUp(firstNode);
+			firstNode.setDown(currentDown);
+			createRow(r+1,c,currentDown);
+		}
+
+	}
+
+	public Cell recorrerMatrix(int i, char c, Cell temp) {    /// 3B
+		try {
+			if(i == temp.getRow()) { /// 1A
+
+				if(c == temp.getCol()) {
+					return temp;
+				}else {
+					temp = temp.getNext();
+					return recorrerMatrix(i, c, temp);
+				}
+			}else {
+				temp = temp.getDown();
+				return recorrerMatrix(i,c,temp);
+			}
+
+		}catch(NullPointerException a) {
+			return null;
+		}
+	}
+
+		public Cell generatePosition(int r, int c) {     // r = 3; c = 5;
+			double w = Math.random()*r+1;
+			double q = Math.random()*c;
+			int rows = (int) w;
+			int b = (int) q;
+			char cols = (char) ('A' + b);
+			return recorrerMatrix(rows,cols,getFirst());
+		}
+
+		public void initializeMirror(int mirror, int a , int b) {
+			setMirrorCon(mirror);
+			setMirror(mirror);
+			setContador(mirror);
+		}
+
+
+
+	}
